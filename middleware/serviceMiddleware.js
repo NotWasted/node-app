7ec1a21a433741ace63d05546816b1618d45a311
harsh <hxt1965@rit.edu>
2021-07-05 20:09:29 +0530

    docker supplementary files

--- middleware/serviceMiddleware.js
+++ middleware/serviceMiddleware.js
@@ -1,1 +1,14 @@
-const tesseract = require('tesseract.js');
+
+VERYFI_CLIENT_ID = "CLIENT_ID"
+ENVIRONMENT_URL = ""
+
+userName = ""
+apiKey = ""
+process_file_url = ""
+
+headers = {
+  "Accept": "application/json",
+  "CLIENT_ID": VERYFI_CLIENT_ID,
+  "AUTHORIZATION": `apikey ${userName}: ${apiKey}`
+}
+
